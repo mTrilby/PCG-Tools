ff556692fb454e6df245c52301eeecc9bcdb9942
mTrilby-innoveo <michael.trilby@innoveo.com>
2021-04-20 01:19:40 +0200

    SplashWindow - small updates

--- AvaloniaPCGTools/ViewModels/SplashWindowViewModel.cs
+++ AvaloniaPCGTools/ViewModels/SplashWindowViewModel.cs
@@ -2,6 +2,8 @@ namespace PCGTools_Avalonia.ViewModels
 {
     public class SplashWindowViewModel : ViewModelBase
     {
-        public string Version { get; } = "My new Version";
+        public string Version { get; } = "3.1.0";
+        public string DevelopedBy { get; } = "Michel Keijzers, MiKeSoft";
+        public string SponsoredBy { get; } = "Traugott";
     }
 }

396b489e0bf21792652a3010e94d83a0387ecbd0
mTrilby-innoveo <michael.trilby@innoveo.com>
2022-04-28 18:34:05 +0200

    {NOTES for initial release}

--- BlazorPCGTools/Program.cs
+++ BlazorPCGTools/Program.cs
@@ -9,3 +9,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+/* TODO:
+    -- MENU: https://code-maze.com/creating-blazor-material-navigation-menu/
+    -- Splash Screen: https://darthpedro.net/2021/03/17/how-to-create-blazor-app-splash-screen/
+    -- (routing): https://docs.microsoft.com/en-us/aspnet/core/blazor/fundamentals/routing?view=aspnetcore-6.0
+ */
+
